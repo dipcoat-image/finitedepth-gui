8930222a0c940db4b3b2583bd1ca9e6ad1bac8a6
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-09-21 09:19:57 +0900

    maint: add files

--- dipcoatimage/finitedepth_gui/__init__.py
+++ dipcoatimage/finitedepth_gui/__init__.py
@@ -4,5 +4,9 @@ GUI package for :mod:`dipcoatimage.finitedepth`.
 
 from dipcoatimage.finitedepth import __version__  # noqa
 
+from .mainwindow import MainWindow
 
-__all__ = []  # type: ignore
+
+__all__ = [
+    "MainWindow",
+]
